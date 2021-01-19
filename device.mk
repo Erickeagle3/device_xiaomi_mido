f93617370eaa1b8fbfcbfd2812146a4e877c00a1
intervigilium <intervigil@gmail.com>
2021-01-19 03:48:00 +0000

    mido: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -392,7 +392,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.basic_mido
 
 # Vibrator
 PRODUCT_PACKAGES += \
